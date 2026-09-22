a5f7b913f50250ff31fffb3a6a565a36f8c33621
JuanJose8282 <josecatalan103@gmail.com>
2026-09-22 12:52:44 -0600

    mAIN PROGRAM
    dADA UNA LISTA DE BARICENTROS LB DE UNA SUPERFICIE CUBULADA, OBTIENES UNA PELICULA DISCRETA DEL

---
+++ DIB001.py
@@ -0,0 +1,11 @@
+#DIB001
+#Entorno 3d con matplotlib
+
+import matplotlib.pyplot as plt
+
+#crea la figura y el eje 3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+#muestra la figura
+plt.show()
